1dc06a0fb286705e5f37715b584974e2a4c89aa8
John C. Welch <jwelch@bynkii.com>
2022-06-09 21:00:20 -0500

    Initial Commit

---
+++ WiFi Analyzer/WiFi_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFi_AnalyzerApp.swift
+//  WiFi Analyzer
+//
+//  Created by John Welch on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WiFi_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
